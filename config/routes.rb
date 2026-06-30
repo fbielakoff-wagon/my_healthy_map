c9d302303f947793ef5d9e5561725be0502ca168
fanny bielakoff <fbielakoff@gmail.com>
2026-06-30 17:03:52 +0100

    Add chat and message creation flow: routes, controllers, views, homepage entry point

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :health_goals
+
+  resources :health_goals do
+    resources :chats, only: [:create]
+  end
+
+    resources :chats, only: [:show] do
+    resources :messages, only: [:create]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
