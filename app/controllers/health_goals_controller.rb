8a242aa2464685368d9246e30e83dd5c9f362796
Daniel Most <daniel@blackmountain.io>
2026-06-30 13:45:22 +0200

    Add health_goals CRUD: routes, controller, and views

--- app/controllers/health_goals_controller.rb
+++ app/controllers/health_goals_controller.rb
@@ -1,2 +1,48 @@
 class HealthGoalsController < ApplicationController
+  def index
+    @health_goals = HealthGoal.all
+  end
+
+  def show
+    @health_goal = HealthGoal.find(params[:id])
+  end
+
+  def new
+    @health_goal = HealthGoal.new
+  end
+
+  def create
+    @health_goal = HealthGoal.new(health_goal_params)
+    @health_goal.user = current_user
+    if @health_goal.save
+      redirect_to health_goal_path(@health_goal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @health_goal = HealthGoal.find(params[:id])
+    @health_goal.destroy
+    redirect_to health_goals_path
+  end
+
+  def edit
+    @health_goal = HealthGoal.find(params[:id])
+  end
+
+  def update
+    @health_goal = HealthGoal.find(params[:id])
+    if @health_goal.update(health_goal_params)
+      redirect_to health_goal_path(@health_goal)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def health_goal_params
+    params.require(:health_goal).permit(:name, :module, :content, :system_prompt)
+  end
 end
