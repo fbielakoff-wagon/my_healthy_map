242768034806c9f2965e968675e9e21d3f0dcd63
fanny bielakoff <fbielakoff@gmail.com>
2026-06-30 11:16:22 +0100

    Add MVC skeleton: models with associations, empty controllers and views

---
+++ app/models/health_goal.rb
@@ -0,0 +1,4 @@
+class HealthGoal < ApplicationRecord
+  belongs_to :user
+  has_many :chats, dependent: :destroy
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_30_100645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "health_goal_id", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["health_goal_id"], name: "index_chats_on_health_goal_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "health_goals", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "module"
+    t.string "name"
+    t.text "system_prompt"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_health_goals_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "attachment_url"
+    t.bigint "chat_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "role"
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "chats", "health_goals"
+  add_foreign_key "chats", "users"
+  add_foreign_key "health_goals", "users"
+  add_foreign_key "messages", "chats"
 end
